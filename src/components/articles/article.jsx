1d79995c40f0e0ae91ab978e9fa3d422fc8928f7
jongmyung <lanto12@naver.com>
2024-01-02 02:45:09 +0900

    :recycle: Refactor: delete add button

--- src/components/articles/article.jsx
+++ src/components/articles/article.jsx
@@ -19,13 +19,6 @@ const Article = (props) => {
 					<div className="article-right-side">
 						<div className="article-title">{title}</div>
 						<div className="article-description">{description}</div>
-						<div className="article-link">
-							Read Article{" "}
-							<FontAwesomeIcon
-								style={{ fontSize: "10px" }}
-								icon={faChevronRight}
-							/>
-						</div>
 					</div>
 				</Link>
 			</div>
